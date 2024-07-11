8310b3f5d819cbd9be523362ab883c901ddd0002
Ana Maria Condori <anacondori@hotmail.com>
2024-07-11 14:04:35 +0200

    Implementar login de usuario con KEYCLOAK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+
+//https://www.npmjs.com/package/keycloak-angular#installation
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8085',
+        realm: 'inventory',
+        clientId: 'angular-client'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        flow: "standard",
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      },
+      loadUserProfileAtStartUp: true
+    });
+}
 
 @NgModule({
   declarations: [
@@ -15,10 +36,16 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     BrowserModule,
     AppRoutingModule,
     DashboardModule,
-
+    KeycloakAngularModule,
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
   ],
   bootstrap: [AppComponent]
 })
